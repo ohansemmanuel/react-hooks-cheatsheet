76887ad2e345b7eac82122d665693deabe43d196
Ohans Emmanuel <ohansemmanuel@gmail.com>
2019-03-31 22:53:49 +0200

    add docz config

---
+++ doczrc.js
@@ -0,0 +1,19 @@
+export default {
+  title: 'Hooks Cheatsheet',
+  description: 'Cheatsheet for React Hooks',
+  repository: 'https://github.com/',
+  indexHtml: 'docs/index.html',
+  ordering: 'ascending',
+  propsParser: false,
+  themeConfig: {
+    repository: 'https://github.com/',
+    colors: {
+      primary: '#bd4932',
+      link: '#bd4932'
+    },
+    logo: {
+      src: 'https://i.imgur.com/kEyxJoQ.png',
+      width: 200
+    }
+  }
+}
