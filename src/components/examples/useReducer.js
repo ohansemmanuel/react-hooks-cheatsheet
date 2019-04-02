4694514d8f6766d3bf3e7dfb1fc62397a9749d4b
Ohans Emmanuel <ohansemmanuel@gmail.com>
2019-04-02 21:27:25 +0200

    useReducer cleanups

--- src/components/examples/useReducer.js
+++ src/components/examples/useReducer.js
@@ -70,27 +70,4 @@ const Bar = () => {
 }
 render(Bar)`
 
-const initialState = { width: 15 }
-const reducer = (state, newState) => ({
-  ...state,
-  newState
-})
-
-const Bar = () => {
-  const [state, setState] = useReducer(reducer, initialState)
-  return (
-    <>
-      <div style={{ background: 'teal', height: '30px', width: state.width }} />
-      <div style={{ marginTop: '3rem' }}>
-        <button onClick={() => setState({ width: 100 })}>
-          Increase bar size
-        </button>
-        <button onClick={() => setState({ width: 3 })}>
-          Decrease bar size
-        </button>
-      </div>
-    </>
-  )
-}
-
 export { BasicExample, InitLazy, ImitateSetState }
