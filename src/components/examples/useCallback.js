7e9401e25bec431deb481df15d7794a80017366b
ohansemmanuel <ohansemmanuel@outlook.com>
2019-04-09 14:04:06 +0200

    complete cheatsheet for useCallback

--- src/components/examples/useCallback.js
+++ src/components/examples/useCallback.js
@@ -1,8 +1,11 @@
-const StarterExample = `
+const Starter = `
 const App = () => {
     const [age, setAge] = useState(99)
     const handleClick = () => setAge(age + 1)
-    const doSomething = () => {}
+    const someValue = "someValue"
+    const doSomething = () => {
+      return someValue
+    }
   
     return (
       <div>
@@ -24,17 +27,96 @@ const Age = ({ age, handleClick }) => {
   )
 }
 
-const Instructions = () => {
+const Instructions = memo((props) => {
   return (
     <div style={{ background: 'black', color: 'yellow', padding: "1rem" }}>
       <p>Follow the instructions above as closely as possible</p>
     </div>
   )
-}
+})
 
 render (
   <App />
 )
 `.trim();
 
-export { StarterExample };
+const ReferencedFn = `
+const App = () => {
+  const [age, setAge] = useState(99)
+  const handleClick = () => setAge(age + 1)
+  const someValue = "someValue"
+  const doSomething = useCallback(() => {
+    return someValue
+  }, [someValue])
+
+  return (
+    <div>
+      <Age age={age} handleClick={handleClick} />
+      <Instructions doSomething={doSomething} />
+    </div>
+  )
+}
+
+const Age = ({ age, handleClick }) => {
+  return (
+    <div>
+      <div style={{ border: '2px', background: "papayawhip", padding: "1rem" }}>
+        Today I am {age} Years of Age
+      </div>
+      <pre> - click the button below 👇 </pre>
+      <button onClick={handleClick}>Get older! </button>
+    </div>
+  )
+}
+
+const Instructions = memo((props) => {
+  return (
+    <div style={{ background: 'black', color: 'yellow', padding: "1rem" }}>
+      <p>Follow the instructions above as closely as possible</p>
+    </div>
+  )
+})
+
+render(<App />)
+`.trim();
+
+const InlineFn = `
+const App = () => {
+  const [age, setAge] = useState(99)
+  const handleClick = () => setAge(age + 1)
+  const someValue = "someValue"
+
+  return (
+    <div>
+      <Age age={age} handleClick={handleClick} />
+      <Instructions doSomething={useCallback(() => {
+        return someValue
+      }, [someValue])} />
+    </div>
+  )
+}
+
+const Age = ({ age, handleClick }) => {
+  return (
+    <div>
+      <div style={{ border: '2px', background: "papayawhip", padding: "1rem" }}>
+        Today I am {age} Years of Age
+      </div>
+      <pre> - click the button below 👇 </pre>
+      <button onClick={handleClick}>Get older! </button>
+    </div>
+  )
+}
+
+const Instructions = memo((props) => {
+  return (
+    <div style={{ background: 'black', color: 'yellow', padding: "1rem" }}>
+      <p>Follow the instructions above as closely as possible</p>
+    </div>
+  )
+})
+
+render(<App />)
+`.trim();
+
+export { Starter, ReferencedFn, InlineFn };
